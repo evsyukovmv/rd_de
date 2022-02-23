fcbea0e2209e5c26bc9f183d6756a64687a993d9
Mykhailo Yevsiukov <evsyukovmv@gmail.com>
2022-02-23 21:45:22 +0200

    hw4 hw5 Fixed URLs, removed folder creation

--- hw5/dags/api_import/api_client/storage.py
+++ hw5/dags/api_import/api_client/storage.py
@@ -11,14 +11,9 @@ class Storage:
         self.__hadoop_client = InsecureClient(**Variable.get('HDFS_CREDENTIALS', deserialize_json=True))
 
     def save(self, name, data):
-        self.__create_directory(name)
-
         path = os.path.join(self.__path(name), self.__key + '.json')
-        with self.__hadoop_client.write(path, encoding='utf-8') as json_file:
+        with self.__hadoop_client.write(path, encoding='utf-8', overwrite=True) as json_file:
             json.dump(data, json_file)
 
     def __path(self, name):
         return os.path.join(self.__config['path'], name)
-
-    def __create_directory(self, name):
-        os.makedirs(self.__path(name), exist_ok=True)
